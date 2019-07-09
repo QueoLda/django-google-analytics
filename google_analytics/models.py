b1a3b5e401ef4ffe4aba68f16d92f2efc6fcbe87
Leopoldo Silva <leopoldo@queo.pt>
2019-07-09 17:42:58 +0100

    Fix model to django 2.2

--- google_analytics/models.py
+++ google_analytics/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 
 class Analytics(models.Model):
-    site = models.ForeignKey(Site)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
     analytics_code = models.CharField(blank=True, max_length=100)
 
     def __unicode__(self):
